022f17df19e5c7f23a2a412b585a1607bd536ed5
Ian Tasker <ianwtasker@outlook.com>
2020-04-09 16:04:23 +0100

    remove unused includes

--- src/SparkTailwindCssPreset.php
+++ src/SparkTailwindCssPreset.php
@@ -3,9 +3,7 @@
 namespace CentralityLabs\SparkTailwindCssPreset;
 
 use Illuminate\Support\Arr;
-use Illuminate\Container\Container;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Foundation\Console\Presets\Preset;
 use Laravel\Ui\Presets\Preset;
 
 class SparkTailwindCssPreset extends Preset
